c92a2aa78ea7586e15ba7f4d148b9a8b06f2987f
madskills700 <madskills700@gmail.com>
2021-03-11 00:14:39 +0300

    Initial commit

---
+++ src/main/java/ru/mirea/trpp_second_5/controllers/MedicineController.java
@@ -0,0 +1,47 @@
+package ru.mirea.trpp_second_5.controllers;
+
+import com.opencsv.bean.CsvToBeanBuilder;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.Optional;
+
+/** Контроллер для работы с медикаментами. */
+@Controller("/medicine")
+public class MedicineController {
+
+    /** Список медикаментов. */
+    private final List<Medicine> medicineList;
+
+    /** Конструктор. */
+    public MedicineController() {
+        medicineList = new CsvToBeanBuilder<Medicine>(new InputStreamReader(this.getClass().getResourceAsStream("/MOCK_DATA.csv"))).withType(Medicine.class).build().parse();
+    }
+
+    /**
+     * Получить список медикаментов.
+     * @return список медикаментов
+     */
+    @Get()
+    public HttpResponse<List<Medicine>> getMedicine() {
+        return HttpResponse.ok(medicineList);
+    }
+
+    /**
+     * Найти медикаменты по идентификатору.
+     * @param id идентификатор медикаментов
+     * @return Медикаменты, если есть, иначе 404 ошибка
+     */
+    @Get("/{id}")
+    public HttpResponse<Medicine> findById(Long id) {
+        Optional<Medicine> result = medicineList.stream().filter(it -> it.getId().equals(id)).findAny();
+        if (result.isPresent()) {
+            return HttpResponse.ok(result.get());
+        } else {
+            return HttpResponse.notFound();
+        }
+    }
+}
---
+++ src/main/java/ru/mirea/trpp_second_5/entity/Medicine.java
@@ -0,0 +1,37 @@
+package ru.mirea.trpp_second_5.entity;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.opencsv.bean.CsvBindByName;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/** Сущность медикаментов. */
+@Getter
+@Setter
+@ToString
+public class Medicine {
+
+    /** Идентификатор. */
+    @JsonProperty("id")
+    @CsvBindByName(column = "id")
+    private Long id;
+    /** Наименование. */
+    @JsonProperty("name")
+    @CsvBindByName(column = "name")
+    private String name;
+    /** Действующее вещество. */
+    @JsonProperty("generic")
+    @CsvBindByName(column = "generic")
+    private String generic;
+    /** Производитель. */
+    @JsonProperty("vendor")
+    @CsvBindByName(column = "vendor")
+    private String vendor;
+    /** Код FDA. */
+    @JsonProperty("fda_code")
+    @CsvBindByName(column = "fda_code")
+    private String fdaCode;
+
+
+}
